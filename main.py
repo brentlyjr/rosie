36511e3ba7b0fc633d5eab89a20a818896343e93
brentlyjr <brentlyjr@yahoo.com>
2023-03-28 15:38:19 -0700

    Updating

--- main.py
+++ main.py
@@ -9,8 +9,17 @@ print("Hello World!")
 api_key = os.environ.get('CHATGPT_KEY')
 print("ChatGPT API KEY: ", api_key)
 
+<<<<<<< Updated upstream
 import whisper
 model = whisper.load_model("base")
 
 result = model.transcribe("harvard.wav")
 result["text"]
+=======
+
+
+
+import openai
+audio_file= open("/path/to/file/audio.mp3", "rb")
+transcript = openai.Audio.transcribe("whisper-1", audio_file)
+>>>>>>> Stashed changes
