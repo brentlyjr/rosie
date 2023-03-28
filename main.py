22aa00904ce22463fecf856b43281b520f5f8064
Alan Shusterman <shusterbaby@gmail.com>
2023-03-28 14:57:30 -0700

    added private key protection

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from .env file
 print("Hello World!")
+
+api_key = os.environ.get('CHATGPT_KEY')
+print("CGPT Key = ",api_key)
