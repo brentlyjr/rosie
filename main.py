9eb5615ffa6ac380e995a25524966750f2c72467
Alan Shusterman <shusterbaby@gmail.com>
2023-03-28 15:07:00 -0700

    fixed api key

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()  # Load environment variables from .env file
 print("Hello World!")
+
+
+api_key = os.environ.get('CHATGPT_KEY')
+print("ChatGPT API KEY: ", api_key)
