218f375e2b1a88c051232731817fcf1f1cefe98f
brentlyjr <brentlyjr@yahoo.com>
2023-03-28 15:38:31 -0700

    Reads a file

--- main.py
+++ main.py
@@ -9,17 +9,10 @@ print("Hello World!")
 api_key = os.environ.get('CHATGPT_KEY')
 print("ChatGPT API KEY: ", api_key)
 
-<<<<<<< Updated upstream
-import whisper
-model = whisper.load_model("base")
-
-result = model.transcribe("harvard.wav")
-result["text"]
-=======
-
-
-
 import openai
-audio_file= open("/path/to/file/audio.mp3", "rb")
+
+audio_file= open("./harvard.wav", "rb")
+openai.api_key = api_key
 transcript = openai.Audio.transcribe("whisper-1", audio_file)
->>>>>>> Stashed changes
+
+print(transcript)
